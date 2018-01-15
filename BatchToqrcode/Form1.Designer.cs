a29d6f6b664b16224082758d1df3fd9a90e048a6
wangfp <wangfp@udui.com.cn>
2018-01-15 11:52:55 +0800

    case start

---
+++ BatchToqrcode/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace BatchToqrcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tburls = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbbgno = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbendno = new System.Windows.Forms.TextBox();
+            this.btrun = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbno = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btrestart = new System.Windows.Forms.Button();
+            this.tbpwd = new System.Windows.Forms.TextBox();
+            this.btpwd = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "网    址：";
+            // 
+            // tburls
+            // 
+            this.tburls.Location = new System.Drawing.Point(108, 44);
+            this.tburls.Name = "tburls";
+            this.tburls.ReadOnly = true;
+            this.tburls.Size = new System.Drawing.Size(134, 21);
+            this.tburls.TabIndex = 1;
+            this.tburls.Text = "http://jump.6glz.com";
+            this.tburls.Click += new System.EventHandler(this.tburls_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "开始编号：";
+            // 
+            // tbbgno
+            // 
+            this.tbbgno.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.tbbgno.Location = new System.Drawing.Point(108, 76);
+            this.tbbgno.Name = "tbbgno";
+            this.tbbgno.Size = new System.Drawing.Size(87, 21);
+            this.tbbgno.TabIndex = 3;
+            this.tbbgno.Text = "51210001";
+            this.tbbgno.Leave += new System.EventHandler(this.tbbgno_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "结束编号：";
+            // 
+            // tbendno
+            // 
+            this.tbendno.Location = new System.Drawing.Point(108, 132);
+            this.tbendno.Name = "tbendno";
+            this.tbendno.ReadOnly = true;
+            this.tbendno.Size = new System.Drawing.Size(134, 21);
+            this.tbendno.TabIndex = 5;
+            this.tbendno.Text = "51210010";
+            // 
+            // btrun
+            // 
+            this.btrun.Location = new System.Drawing.Point(36, 200);
+            this.btrun.Name = "btrun";
+            this.btrun.Size = new System.Drawing.Size(206, 23);
+            this.btrun.TabIndex = 6;
+            this.btrun.Text = "批量生成";
+            this.btrun.UseVisualStyleBackColor = true;
+            this.btrun.Click += new System.EventHandler(this.btrun_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(257, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "默认图片生成在程序同目录下，297PX，PNG格式";
+            // 
+            // tbno
+            // 
+            this.tbno.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.tbno.Location = new System.Drawing.Point(108, 105);
+            this.tbno.Name = "tbno";
+            this.tbno.Size = new System.Drawing.Size(134, 21);
+            this.tbno.TabIndex = 9;
+            this.tbno.Text = "10";
+            this.tbno.Leave += new System.EventHandler(this.tbno_Leave);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "数    量：";
+            // 
+            // btrestart
+            // 
+            this.btrestart.Location = new System.Drawing.Point(202, 76);
+            this.btrestart.Name = "btrestart";
+            this.btrestart.Size = new System.Drawing.Size(40, 23);
+            this.btrestart.TabIndex = 10;
+            this.btrestart.Text = "恢复";
+            this.btrestart.UseVisualStyleBackColor = true;
+            this.btrestart.Click += new System.EventHandler(this.btrestart_Click);
+            // 
+            // tbpwd
+            // 
+            this.tbpwd.Location = new System.Drawing.Point(108, 15);
+            this.tbpwd.Name = "tbpwd";
+            this.tbpwd.Size = new System.Drawing.Size(87, 21);
+            this.tbpwd.TabIndex = 11;
+            this.tbpwd.Text = "输入解锁密码";
+            this.tbpwd.Click += new System.EventHandler(this.tbpwd_Click);
+            // 
+            // btpwd
+            // 
+            this.btpwd.Location = new System.Drawing.Point(201, 15);
+            this.btpwd.Name = "btpwd";
+            this.btpwd.Size = new System.Drawing.Size(40, 23);
+            this.btpwd.TabIndex = 12;
+            this.btpwd.Text = "恢复";
+            this.btpwd.UseVisualStyleBackColor = true;
+            this.btpwd.Click += new System.EventHandler(this.btpwd_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(183, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "@CopyRignt Wfp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btpwd);
+            this.Controls.Add(this.tbpwd);
+            this.Controls.Add(this.btrestart);
+            this.Controls.Add(this.tbno);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btrun);
+            this.Controls.Add(this.tbendno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbbgno);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tburls);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "批量生成二维码";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tburls;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbbgno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbendno;
+        private System.Windows.Forms.Button btrun;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbno;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btrestart;
+        private System.Windows.Forms.TextBox tbpwd;
+        private System.Windows.Forms.Button btpwd;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BatchToqrcode/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.QrCodeNet.Encoding;
+using System.Drawing.Imaging;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace BatchToqrcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tbpwd.Visible = false;
+            btpwd.Visible = false;
+            tburls.ReadOnly = true;
+            
+        }
+
+
+        private void tburls_Click(object sender, EventArgs e)
+        {
+            if (tburls.ReadOnly == true)
+            {
+                tbpwd.Visible = true;
+                btpwd.Visible = true;
+            }
+           
+        }
+
+        private void tbpwd_Click(object sender, EventArgs e)
+        {
+            tbpwd.Text = "";
+        }
+
+        private void btpwd_Click(object sender, EventArgs e)
+        {
+            if (tbpwd.Visible)
+            {
+                if (tbpwd.Text == "")
+                {
+                    MessageBox.Show("解锁密码不能为空");
+                    tbpwd.Focus();
+                    return;
+                }
+
+                if (tbpwd.Text.Length < 6)
+                {
+                    MessageBox.Show("解锁密码太短");
+                    tbpwd.Focus();
+                    return;
+                }
+
+                string input = tbpwd.Text.Trim();
+
+                byte[] result = Encoding.Default.GetBytes(input);    //tbPass为输入密码的文本框
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] output = md5.ComputeHash(result);
+                string pwdstr = BitConverter.ToString(output).Replace("-", "");  //tbMd5pass为输出加密文本的文本框
+
+
+                if (pwdstr == "C8C2D00CE9CA0FFDF6B07D41B1B79858")
+                {
+                    tburls.ReadOnly = false;
+                    tbpwd.Visible = false;
+                    btpwd.Visible = false;
+                }
+                else {
+                    MessageBox.Show("解锁密码有误");
+                    tbpwd.Focus();
+                    return;
+                }
+
+            }
+            else {
+                MessageBox.Show("非法解锁");
+                btpwd.Visible = false;
+                return;
+            }
+           
+        }
+
+       
+
+        private void tbbgno_Leave(object sender, EventArgs e)
+        {
+            if (tbbgno.Text == "")
+            {
+                MessageBox.Show("起始编号不能为空");
+                tbbgno.Focus();
+                return;
+            }
+
+            string pattern = @"^\d*$";
+            if (!Regex.IsMatch(tbbgno.Text, pattern)) {
+                MessageBox.Show("起始编号为正整数");
+                tbbgno.Focus();
+                tbbgno.Text = "";
+                return;
+            }
+
+
+            if (Convert.ToInt32(tbbgno.Text) == 0)
+            {
+                MessageBox.Show("起始编号不能为零");
+                tbbgno.Focus();
+                tbbgno.Text = "";
+                return;
+            }
+
+
+            if (Convert.ToInt32(tbbgno.Text) < 0)
+            {
+                MessageBox.Show("起始编号应大于零");
+                tbbgno.Focus();
+                tbbgno.Text = "";
+                return;
+            }  
+
+            Int32 bgno = Convert.ToInt32(tbbgno.Text);
+            int no = Convert.ToInt32(tbno.Text);
+
+            tbendno.Text = (bgno + no-1).ToString();
+        }
+
+        private void tbno_Leave(object sender, EventArgs e)
+        {
+            if (tbno.Text == "")
+            {
+                MessageBox.Show("数量不能为空");
+                tbno.Focus();
+                return;
+            }
+
+            string pattern = @"^\d*$";
+            if (!Regex.IsMatch(tbno.Text, pattern))
+            {
+                MessageBox.Show("数量为正整数");
+                tbno.Focus();
+                tbno.Text = "";
+                return;
+            }
+
+            if (Convert.ToInt32(tbno.Text) == 0)
+            {
+                MessageBox.Show("数量不能为零");
+                tbno.Focus();
+                tbno.Text = "";
+                return;
+            }
+
+
+            if (Convert.ToInt32(tbno.Text) < 0)
+            {
+                MessageBox.Show("数量应大于零");
+                tbno.Focus();
+                tbno.Text = "";
+                return;
+            }
+
+
+            Int32 bgno = Convert.ToInt32(tbbgno.Text);
+            int no = Convert.ToInt32(tbno.Text);
+
+            tbendno.Text = (bgno + no-1).ToString();
+        }
+
+        private void btrestart_Click(object sender, EventArgs e)
+        {
+            tbbgno.Text = "51210001";
+            Int32 bgno = Convert.ToInt32(tbbgno.Text);
+            int no = Convert.ToInt32(tbno.Text);
+
+            tbendno.Text = (bgno + no-1).ToString();
+        }
+
+        private void btrun_Click(object sender, EventArgs e)
+        {
+            Int32 bgno = Convert.ToInt32(tbbgno.Text);
+            Int32 endno = Convert.ToInt32(tbendno.Text);
+            for (int i = bgno; i <= endno; i++) {
+                createQrCode(tburls.Text + "?b=" + i,i);
+            }
+        }
+
+        /// <summary>
+        /// 生成二维码的方法
+        /// </summary>
+        /// <param name="str">二维码包含的内容</param>
+        /// <param name="moduleSize">二维码大小</param>
+        /// <param name="no">编号</param>
+        private void createQrCode(string content, int no, int moduleSize = 9)
+        {
+            //QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            //QrCode qrCode = new QrCode();
+            //qrEncoder.TryEncode(str, out qrCode);
+
+            string filename = DateTime.Now.ToString("d");
+            filename = filename.Replace("/", "");
+            filename = filename.Replace(":", "");
+            filename = filename.Replace("-", "");
+            filename = filename.Replace(".", "");
+            filename = filename.Replace(" ", "");
+            string filepath = System.AppDomain.CurrentDomain.BaseDirectory + filename ;
+
+            if (!Directory.Exists(filepath))
+                Directory.CreateDirectory(filepath);
+
+            string fileName = filepath + "\\" + no + ".png";
+            //Renderer renderer = new Renderer(5, Brushes.Black, Brushes.White);
+            //renderer.CreateImageFile(qrCode.Matrix, @filename, ImageFormat.Png);
+
+
+
+
+
+            //ErrorCorrectionLevel 误差校正水平
+            //QuietZoneModules     空白区域
+
+            var encoder = new QrEncoder(ErrorCorrectionLevel.M);
+            QrCode qrCode = encoder.Encode(content);
+            GraphicsRenderer render = new GraphicsRenderer(new FixedModuleSize(moduleSize, QuietZoneModules.Two), Brushes.Black, Brushes.White);
+            
+            //MemoryStream memoryStream = new MemoryStream();
+            //render.WriteToStream(qrCode.Matrix, ImageFormat.Jpeg, memoryStream);
+            
+            //return memoryStream;
+
+            //生成图片的代码
+            DrawingSize dSize = render.SizeCalculator.GetSize(qrCode.Matrix.Width);
+            Bitmap map = new Bitmap(dSize.CodeWidth, dSize.CodeWidth);
+            Graphics g = Graphics.FromImage(map);
+            render.Draw(g, qrCode.Matrix);
+            map.Save(fileName, ImageFormat.Png);//fileName为存放的图片路径
+
+
+
+
+        }
+
+        
+    }
+}
